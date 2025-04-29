719d2fcdba5000a0a85ddff0fcc4b88c657b56f3
Izar17 <jhomar.izar@gmail.com>
2025-04-29 21:21:14 +0800

    update hosting

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ if(!defined('base_url')) define('base_url','https://makantransport.site/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"transport_db");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"u187136132_transport");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Alliswell17*");
+if(!defined('DB_NAME')) define('DB_NAME',"u187136132_transport");
 ?>
