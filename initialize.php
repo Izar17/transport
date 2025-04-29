55b490ad7196420db03f055dd5ab537d2287f789
Izar17 <jhomar.izar@gmail.com>
2025-04-29 21:29:08 +0800

    Remove dev data

--- initialize.php
+++ initialize.php
@@ -1,8 +1,6 @@
 <?php
-$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'Izar','username'=>'','password'=>'','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','https://makantransport.site/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
-if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"u187136132_transport");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Alliswell17*");
