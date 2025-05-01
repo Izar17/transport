667fdccf7cb3937aa0b0ae430ba78082b17edcfb
Izar17 <jhomar.izar@gmail.com>
2025-05-02 02:46:04 +0800

    update env

--- initialize.php
+++ initialize.php
@@ -1,8 +1,8 @@
 <?php
-if(!defined('base_url')) define('base_url','http://localhost/transport/');
+if(!defined('base_url')) define('base_url','https://makantransport.site/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"transport_db");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"u187136132_transport");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Alliswell17*");
+if(!defined('DB_NAME')) define('DB_NAME',"u187136132_transport");
 ?>
