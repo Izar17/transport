1d172bd43c65cefdb0e0b95d04f29fd918baee52
Izar17 <jhomar.izar@gmail.com>
2025-05-07 00:50:33 +0800

    update db connection

--- admin/schedule/db-connect.php
+++ admin/schedule/db-connect.php
@@ -1,8 +1,8 @@
 <?php
 $host     = 'localhost';
-$username = 'root';
-$password = '';
-$dbname   ='calendar_db';
+$username = 'u187136132_transport';
+$password = 'Alliswell17*';
+$dbname   ='u187136132_transport';
 
 $conn = new mysqli($host, $username, $password, $dbname);
 if(!$conn){
