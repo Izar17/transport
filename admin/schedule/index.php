faebb98c1f2167c7741e19db8faa0218e304d49f
Izar17 <jhomar.izar@gmail.com>
2025-05-07 00:54:07 +0800

    remove font

--- admin/schedule/index.php
+++ admin/schedule/index.php
@@ -22,7 +22,7 @@
         body {
             height: 100%;
             width: 100%;
-            font-family: Apple Chancery, cursive;
+            /* font-family: Apple Chancery, cursive; */
         }
 
         .btn-info.text-light:hover,
